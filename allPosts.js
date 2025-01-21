56267326bf256a49cc462f0097d79f89abc193a5
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-21 15:47:17 +0100

    feat: control id (number or string) error (400-404)

---
+++ allPosts.js
@@ -0,0 +1,38 @@
+let posts = [
+    {
+        id: 1,
+        title: "Introduzione a HTML: La Base del Web",
+        content: "Scopri cos'è HTML, come funziona e come creare la tua prima pagina web con i tag fondamentali.",
+        image: `http://localhost:3000/img/ciambellone.jpeg`,
+        tags: ["HTML", "web development", "frontend", "tutorial"]
+    },
+    {
+        id: 2,
+        title: "Guida Completa a CSS: Stile e Layout",
+        content: "Impara come usare CSS per migliorare l'aspetto delle tue pagine web, dai colori ai layout responsivi.",
+        image:  `http://localhost:3000/img/cracker_barbabietola.jpeg`,
+        tags: ["CSS", "design", "frontend", "stili"]
+    },
+    {
+        id: 3,
+        title: "JavaScript: I Fondamenti per Principianti",
+        content: "Scopri le basi di JavaScript, il linguaggio di programmazione più usato nel web moderno.",
+        image:  `http://localhost:3000/img/pane_fritto_dolce.jpeg`,
+        tags: ["JavaScript", "frontend", "programmazione", "interattività"]
+    },
+    {
+        id: 4,
+        title: "Introduzione a Node.js: JavaScript sul Server",
+        content: "Cos'è Node.js? Scopri come eseguire JavaScript lato server e creare applicazioni veloci e scalabili.",
+        image:  `http://localhost:3000/img/pasta_barbabietola.jpeg`,
+        tags: ["Node.js", "backend", "JavaScript", "server"]
+    },
+    {
+        id: 5,
+        title: "Express.js: Creare un'API REST in pochi passi",
+        content: "Impara a usare Express.js per creare API RESTful e gestire le richieste HTTP in modo efficiente.",
+        image:  `http://localhost:3000/img/torta_paesana.jpeg`,
+        tags: ["Express.js", "backend", "API", "Node.js"]
+    }]
+
+    module.exports = posts
--- routers/posts.js
+++ routers/posts.js
@@ -1,28 +1,72 @@
 const express = require("express")
 const router = express.Router()
 
+// IMPORTAZIONE ARRAY POSTS
+const postsData = require("../allPosts")
+
 // Index
 router.get('/', function (req, res) {
-    res.json(`Lista dei post`);
+    res.json(`Lista dei post`); 
 });
+
 // Show
 router.get('/:id', function (req, res) {
+    if(isNaN(req.params.id)){
+        return res.sendStatus(400)
+    }
+
+    const post = postsData.find((elm) => elm.id == req.params.id)
+
+    if (!post){
+       res.sendStatus(404)
+    }   
     res.json(`Dettagli di un post ${req.params.id}`);
 });
+
 // Create
 router.post('/', function (req, res) {
     res.json(`Creazione nuovo post`);
 });
+
 // Update
 router.put('/:id', function (req, res) {
+    if(isNaN(req.params.id)){
+        return res.sendStatus(400)
+    }
+
+    const post = postsData.find((elm) => elm.id == req.params.id)
+
+    if (!post){
+       res.sendStatus(404)
+    } 
     res.json(`Modifica integrale del post ${req.params.id} `);
 });
+
 // Modify
 router.patch('/:id', function (req, res) {
+    if(isNaN(req.params.id)){
+        return res.sendStatus(400)
+    }
+
+    const post = postsData.find((elm) => elm.id == req.params.id)
+
+    if (!post){
+       res.sendStatus(404)
+    } 
     res.json(`Modifica parziale del post ${req.params.id} `);
 });
+
 // Delete
 router.delete('/:id', function (req, res) {
+    if(isNaN(req.params.id)){
+        return res.sendStatus(400)
+    }
+
+    const post = postsData.find((elm) => elm.id == req.params.id)
+
+    if (!post){
+       res.sendStatus(404)
+    } 
     res.json(`Eliminazione del post ${req.params.id} `);
 });
 
